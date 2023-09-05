68a8e9b2507e624ef5439e4f9c0b18fff45f7902
Gagan Shenvi <107752760+imgagan18@users.noreply.github.com>
2023-09-06 00:15:29 +0530

    Update Conditional.java

--- Conditional.java
+++ Conditional.java
@@ -1,18 +1,18 @@
-// import java.util.*;
-// public class Conditional {
-//     public static void main(String[] args) {
-//         Scanner sc= new Scanner(System.in); 
-//         System.out.print("Enter first number- ");  
-//         int a= sc.nextInt();  
-//         // System.out.print("Enter second number- ");  
-//         // int b= sc.nextInt(); 
+import java.util.*;
+public class Conditional {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in); 
+        System.out.print("Enter first number- ");  
+        int a= sc.nextInt();  
+        // System.out.print("Enter second number- ");  
+        // int b= sc.nextInt(); 
 
-//         if (a%2==0){
-//             System.out.println( "the number is even");
-//         }
-//           else{
-//         System.out.println(" number is odd");
-//               }
-//     }
+        if (a%2==0){
+            System.out.println( "the number is even");
+        }
+          else{
+        System.out.println(" number is odd");
+              }
+    }
     
-// }
+}
