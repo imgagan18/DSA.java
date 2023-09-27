b4427cc3e76ebac6399aa479c668e2dbeabcadba
Gagan Shenvi <shenvigagan@gmail.com>
2023-09-27 23:56:57 +0530

    selection sort

---
+++ selection.java
@@ -0,0 +1,25 @@
+public class selection {
+
+    public static void PrintArray(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+    public static void main(String[] args) {
+       int arr[]={9,2,4,5,5,1};
+
+       //selection sort
+
+       for(int i=0;i<arr.length;i++){
+        int smallest=i;
+
+        for (int j=0; j<arr.length;j++){
+            if(arr[smallest]>arr[i]){
+                smallest=j;
+            }
+        }
+       }
+       PrintArray(arr);
+
+    }
+}
