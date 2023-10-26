cc4b847a619da734101c3c0092aa9ef0617de170
Gagan Shenvi <shenvigagan@gmail.com>
2023-10-26 19:49:37 +0530

    bubble

--- bubble.java
+++ bubble.java
@@ -1,24 +1,24 @@
 public class bubble {
-
-    public static void printArray(int arr[]) {
-        for (int i = 0; i < arr.length; i++)
-            System.out.println(arr[i]);
-    }
-
-    public static void main(String[] args) {
-        int arr[] = { 7, 8, 2, 3, 1 };
-
-        // bubble sort
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length - i - 1; j++) {
-                // swap
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
+    public static void bubbleSort(int arr[]){
+        for(int turn=0; turn<arr.length; turn++){
+          for(int j=0; j<arr.length-1; j++){
+            if(arr[j]>arr[j+1]){
+              int temp=arr[j];
+              arr[j]=arr[j+1];
+              arr[j+1]=temp;
             }
+          }
         }
-       printArray(arr);
     }
+        
+    public static void Printarray(int arr[]){
+        for(int i=0; i<arr.length; i++){
+          System.out.print(arr[i]+" ");
+        }
+    }
+  public static void main(String[] args) {
+    int arr[]={9,2,4,5,5,1};
+    bubbleSort(arr);
+    Printarray(arr);
+  }
 }
