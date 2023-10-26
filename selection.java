1d6abcf7b016eed09273e46eb9dcb99a75f6a731
Gagan Shenvi <shenvigagan@gmail.com>
2023-10-26 20:26:48 +0530

    selection

--- selection.java
+++ selection.java
@@ -1,29 +1,30 @@
 public class selection {
 
-    public static void PrintArray(int arr[]){
+    public static void Printarray(int arr[]){
         for(int i=0;i<arr.length;i++){
-            System.out.println(arr[i]);
+         System.out.println(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void selection(int arr[]){
+        for(int i=0 ; i<arr.length-1; i++){
+            int minpos=i;
+            for(int j= i+1; j<arr.length ; j++){
+                if(arr[minpos] > arr[j]) {
+                    minpos=j;
+                }
+            }
+            //swap
+
+            int temp=arr[minpos];
+            arr[minpos]=arr[i];
+            arr[i]=temp;
         }
     }
+
     public static void main(String[] args) {
-       int arr[]={9,2,4,5,5,1};
-
-       //selection sort
-
-       for(int i=0;i<arr.length;i++){
-        int smallest=i;
-
-        for (int j=0; j<arr.length;j++){
-            if(arr[smallest]>arr[i]){
-                smallest=j;
-            }
-        }
-
-        int temp=arr[smallest];
-        arr[smallest]=arr[i];
-        arr[i]=temp;
-       }
-       PrintArray(arr);
-
+        int arr[]={2,3,4,4,7,9,29};
+        selection(arr);
+         Printarray(arr);
     }
 }
