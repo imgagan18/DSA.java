249049025b71db485910f3f7d533a73731dd4117
Gagan Shenvi <shenvigagan@gmail.com>
2023-11-01 22:32:08 +0530

    inbuilt

---
+++ inbuiltSort.java
@@ -0,0 +1,8 @@
+public class inbuiltSort {
+    public static void Printarray(int arr[]){
+        for(int i=0;i<arr.length;i++){
+         System.out.println(arr[i]+" ");
+        }
+        System.out.println();
+    }
+}
