d517e682ee0625fd86732ede24a0eb2a5b089113
Gagan Shenvi <shenvigagan@gmail.com>
2023-11-02 22:37:01 +0530

    inbuilt sort

--- inbuiltSort.java
+++ inbuiltSort.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class inbuiltSort {
     public static void Printarray(int arr[]){
         for(int i=0;i<arr.length;i++){
@@ -5,4 +6,13 @@ public class inbuiltSort {
         }
         System.out.println();
     }
+    public static void main(String[] args) {
+        System.out.println();
+        int arr[]={4,3,4,5,6,3,2,4,5,6,67,7,5};
+        Arrays.sort(arr);// this is the important line of the code ...
+        Printarray(arr);
+    }
 }
+
+
+//0(nlogn) tine complexity
