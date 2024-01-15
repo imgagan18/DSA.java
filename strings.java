82149058cf9e94489e74093622ac2b883b733f0c
Gagan Shenvi <shenvigagan@gmail.com>
2024-01-15 18:04:35 +0530

    strings

--- strings.java
+++ strings.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
+
 public class strings {
     public static void main(String[] args) {
         char arr[]={'a','b','c','d'};
         String str="abcd";
         String str2= new String("xyz");
          
+        // strings are IMMUTABLE
+         
+        Scanner sc= new Scanner(System.in);
+        String name;
+        name=sc.next();
+        System.out.println(name);
+        sc.next();
     }
 }
