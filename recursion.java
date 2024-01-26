58be385f7536f28fe23921e5b57dc502c3f7b5c2
Gagan Shenvi <shenvigagan@gmail.com>
2024-01-26 17:02:40 +0530

    recurssion

--- recursion.java
+++ recursion.java
@@ -1,19 +1,35 @@
 // public class recursion {
-//     public static void printDec(int n){
-//         if(n==1){
-//             System.out.print(n);
-//             return;
+//      public static void main(String[] args) 
+//      {
+//         print(10);
+       
+//      }
+//      static int print(int n)
+//      {
+//         if(n==0)
+//         {
+//             return n;
 //         }
-//         System.out.print(n+" ");
-//         printDec(n-1);
-//     }
-//     public static void main(String[] args) {
-//         int n=10;
-//         printDec(n);
+//         {
+//             // print(n-1);   
+
+//         System.out.println(n);
+//         return print(n-1);   
+//         }
 //     }
 // }
 
-
 public class recursion {
-    
+    public static void PrintDec(int n){
+        if(n==0){
+            System.out.println(n); 
+            return ; 
+        }
+        System.out.println(n);
+        PrintDec(n-1);
+    }
+    public static void main(String[] args) {
+        int n=10;
+        PrintDec(n);
+    }
 }
