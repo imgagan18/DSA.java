db9b1a88cab0bd540eebc083ac1157d3684bc53c
Gagan Shenvi <shenvigagan@gmail.com>
2024-04-01 18:47:12 +0530

    array list

--- arrayList.java
+++ arrayList.java
@@ -1,22 +1,64 @@
-class arrayList{
+import java.util.ArrayList;
+import java.util.List;
 
-    public static void Swap(int arr[],int index1,int index2){ 
-       int temp=arr[index1];
-        arr[index1]=arr[index2];
-        arr[index2]=temp;
+class arrayList {
+
+  public static int storeWater(ArrayList<Integer> height) {
+    int maxWater = 0;
+    for (int i = 0; i < height.size(); i++) {
+      for (int j = i + 1; j < height.size(); j++) {
+        int ht = Math.min(height.get(i), height.get(j));
+        int widht = i - j;
+        int currentWater = ht * widht;
+        maxWater = Math.max(maxWater, currentWater);
+      }
+
     }
-    public static void main(String[] args) {
-        int numbers[]={3,4,5,6,7,8};
-        System.out.println(numbers);
-        int index1=2;
-        int index2=4;
-        Swap(numbers, index1, index2);
+    return maxWater;
+  }
 
-        for (int num : numbers) {
-            System.out.print(num + " ");
+  // based on 2 pointer approach(pair sum -1)
+  public static boolean pointer(ArrayList<Integer>list,int target){
+         int lp=0;
+         int rp=list.size()-1;
+
+         while(lp!=rp){
+           if(list.get(lp)+list.get(rp)==target){
+            return true;
+           }
+           if(list.get(lp)+list.get(rp)<target){
+            lp++;
+           }
+           else{
+            rp--;
+           }
+         }
+         return false;
+            }
+
+  public static boolean Pairsum(ArrayList<Integer> list, int target) {
+    for (int i = 0; i < list.size(); i++) {
+      for (int j = i + 1; j < list.size(); j++) {
+        if (list.get(i) + list.get(j) == target) {
+          return true;
         }
-
-        
-
+      }
     }
+    return false;
+  }
+
+  public static void main(String[] args) {
+    ArrayList<Integer> list = new ArrayList<>();
+    // [1,8,6,2,5,4,8,3,7]
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    list.add(4);
+    list.add(5);
+    list.add(6);
+    
+    
+    int target = 5;
+    System.out.println(pointer(list, target));
+  }
 }
